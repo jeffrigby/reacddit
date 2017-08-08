ea5bf5dae28168b42070173599ef82da6bcad8c3
Jeffrey Rigby <jrigby@mac.com>
2017-08-08 17:01:19 -0400

    Removed hot plug-in. Webpack 3

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
   ],
   output: {
     path: path.join(__dirname, '/dist/'),
-    filename: '[name].js',
+    filename: '[name].[hash].js',
     publicPath: '/',
   },
   context: path.join(__dirname, 'src'),
