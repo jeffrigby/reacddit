2a61912b1d2e928750cbe8488cfa39c09079a9bf
Jeffrey Rigby <jrigby@mac.com>
2018-05-14 15:40:18 -0400

    Forgot to add

--- src/redux/actions/subreddits.js
+++ src/redux/actions/subreddits.js
@@ -1,6 +1,4 @@
 import axios from 'axios';
-// import { redditRefreshAuth } from './reddit';
-// import Reddit from '../../reddit/reddit';
 import RedditHelpers from '../../reddit/redditHelpers';
 
 
