7dffe1d71fe664bd153e796d01a358a4ce7be41c
Jeffrey Rigby <jrigby@mac.com>
2019-03-07 17:28:23 -0500

    imgur support for mp4 links

--- src/components/posts/embeds/domains/imgurcom.js
+++ src/components/posts/embeds/domains/imgurcom.js
@@ -3,6 +3,23 @@ import redditImagePreview from '../defaults/redditImagePreview';
 import redditVideoPreview from '../defaults/redditVideoPreview';
 
 const render = entry => {
+  const parsedUrl = parse(entry.url, true);
+  const { pathname } = parsedUrl;
+  const cleanedPath = pathname
+    .substring(1)
+    .replace(/\/new$/, '')
+    .replace(/^\/|\/$/g, '');
+
+  // Try to get width and height
+  let width = 1024;
+  let height = 768;
+
+  if (entry.preview) {
+    const { width: w, height: h } = entry.preview.images[0].source;
+    width = w;
+    height = h;
+  }
+
   // Fallback video content
   try {
     const video = redditVideoPreview(entry);
@@ -15,6 +32,29 @@ const render = entry => {
   } catch (e) {
     // eslint-disable-next-line no-console
     console.error(e);
+  }
+
+  // Check for gifv content;
+  if (cleanedPath.match(/gifv$/)) {
+    const mp4Filename = cleanedPath.replace(/gifv$/, 'mp4');
+    const mp4 = `https://i.imgur.com/${mp4Filename}`;
+    return {
+      type: 'video',
+      width,
+      height,
+      mp4,
+    };
+  }
+
+  // Check for mp4 content
+  if (cleanedPath.match(/mp4$/)) {
+    const mp4 = `https://i.imgur.com/${cleanedPath}`;
+    return {
+      type: 'video',
+      width,
+      height,
+      mp4,
+    };
   }
 
   // Check for preview image:
@@ -35,16 +75,6 @@ const render = entry => {
     return null;
   }
 
-  const parsedUrl = parse(entry.url, true);
-  const { pathname } = parsedUrl;
-  const cleanedPath = pathname
-    .substring(1)
-    .replace(/\/new$/, '')
-    .replace(/^\/|\/$/g, '');
-
-  // Try to get width and height
-  const { width, height } = entry.preview.images[0].source;
-
   const id = cleanedPath.split('.')[0];
 
   const src = `https://i.imgur.com/${id}h.jpg`;
@@ -60,21 +90,3 @@ const render = entry => {
 };
 
 export default render;
-
-/** *
- private function _get_imgurId($url) {
-        $url = rtrim($url, '/');
-        $url = preg_replace('/\/new$/', '', $url);
-        $pathinfo = pathinfo($url);
-        $parse_url = parse_url($pathinfo['dirname']);
-        $imgur_id = parse_url($pathinfo['filename']);
-        $imgur_id = $imgur_id['path'];
-        if (empty($imgur_id)) {
-            return FALSE;
-        }
-        return array(
-            'id' => $imgur_id,
-            'type' => !empty($parse_url['path']) ? trim($parse_url['path'], '/') : NULL
-        );
-    }
- * */
