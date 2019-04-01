6852b85a9bd8f3d0f76ccbddc9514d3f65c2e798
Jeffrey Rigby <jrigby@mac.com>
2019-04-01 12:10:25 -0400

    Shorten links

--- src/components/posts/contentTypes/Self.js
+++ src/components/posts/contentTypes/Self.js
@@ -2,12 +2,39 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Content from '../Content';
 
-const Self = ({ content, load, name }) => {
-  let rawhtml = content.html;
+const cleanLinks = html => {
+  let rawhtml = html;
   rawhtml = rawhtml
     .replace(/<a\s+href=/gi, '<a target="_blank" href=')
     .replace(/<p>&#x200B;<\/p>/gi, '')
     .replace(/<p>\s+<\/p>/gi, '');
+
+  // Shorten all text in anchor tags
+  const regex = /<a [^>]+>(https?:\/\/[^>]+)<\/a>/gm;
+  const matches = [];
+  let match = regex.exec(rawhtml);
+  while (match !== null) {
+    matches.push(match[1]);
+    match = regex.exec(rawhtml);
+  }
+
+  if (matches.length > 0) {
+    matches.forEach(link => {
+      if (link.length >= 20) {
+        const newLink = `${link
+          .replace(/https?:\/\//, '')
+          .substring(0, 25)}...`;
+        rawhtml = rawhtml.replace(`>${link}<`, `>${newLink}<`);
+      }
+    });
+  }
+
+  return rawhtml;
+};
+
+const Self = ({ content, load, name }) => {
+  const rawhtml = cleanLinks(content.html);
+
   // eslint-disable-next-line react/no-danger
   const html = <div dangerouslySetInnerHTML={{ __html: rawhtml }} />;
   let inlineRendered;
