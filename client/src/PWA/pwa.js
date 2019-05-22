d4ace30498532b8c5fb7874e02c51be549b24bc0
Jeffrey Rigby <jrigby@mac.com>
2019-05-22 11:01:42 -0400

    Extracted PWA reload code to separate file.

---
+++ client/src/PWA/pwa.js
@@ -0,0 +1,28 @@
+// this is a file
+const refresh = () => {
+  if ('serviceWorker' in navigator) {
+    if (caches) {
+      // Service worker cache should be cleared with caches.delete()
+      caches.keys().then(names => {
+        for (let name of names) caches.delete(name);
+      });
+    }
+
+    navigator.serviceWorker.ready.then(registration => {
+      registration.unregister();
+      window.location.reload(true);
+    });
+  } else {
+    window.location.reload(true);
+  }
+};
+
+setTimeout(() => {
+  try {
+    document.getElementById('error-loading').classList.remove('d-none');
+    document.getElementById('landing-icon').classList.add('d-none');
+    document
+      .getElementById('force-refresh')
+      .addEventListener('onclick', refresh);
+  } catch (e) {}
+}, 5000);
