7bae38ada352772609ac7ccab2ef05889c7475b2
Jeffrey Rigby <9286474+jeffrigby@users.noreply.github.com>
2019-05-29 13:53:39 -0400

    Fix for bad service worker URL

--- client/src/serviceWorker.js
+++ client/src/serviceWorker.js
@@ -36,7 +36,7 @@ export function register(config) {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `${process.env.PROD_PUBLIC_PATH}/service-worker.js`;
+      const swUrl = `${process.env.CLIENT_PATH}/service-worker.js`;
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
