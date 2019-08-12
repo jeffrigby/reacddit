9132b6cb44fddeca81d6fe902b8a3de7492adc59
Jeffrey Rigby <9286474+jeffrigby@users.noreply.github.com>
2019-08-12 00:33:11 -0400

    Limit width of iframes.

--- client/src/components/posts/contentTypes/IFrame16x9.js
+++ client/src/components/posts/contentTypes/IFrame16x9.js
@@ -3,18 +3,20 @@ import PropTypes from 'prop-types';
 
 const IFrame16x9 = ({ content, load }) => {
   return (
-    <div className="media-cont">
-      <div className="embed-responsive embed-responsive-16by9 black-bg">
-        {load && (
-          <iframe
-            src={content.src}
-            scrolling="no"
-            title={content.name}
-            allow={content.allow}
-            className="embed-responsive-item"
-            allowFullScreen
-          />
-        )}
+    <div className="media-cont black-bg">
+      <div className="media-contain-width">
+        <div className="embed-responsive embed-responsive-16by9 black-bg">
+          {load && (
+            <iframe
+              src={content.src}
+              scrolling="no"
+              title={content.name}
+              allow={content.allow}
+              className="embed-responsive-item"
+              allowFullScreen
+            />
+          )}
+        </div>
       </div>
     </div>
   );
