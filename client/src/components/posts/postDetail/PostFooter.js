af4e65e85307f98f53ec69def25aa0b8c4755990
Jeffrey Rigby <9286474+jeffrigby@users.noreply.github.com>
2019-11-07 14:51:26 -0500

    Added click-to-copy to ID and a new link to isolate

--- client/src/components/posts/postDetail/PostFooter.js
+++ client/src/components/posts/postDetail/PostFooter.js
@@ -1,12 +1,14 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import copy from 'copy-to-clipboard';
 import PostByline from './PostByline';
 import { PostsContextData } from '../../../contexts';
 
 const PostFooter = ({ debug, visible, toggleShowDebug }) => {
   const data = useContext(PostsContextData);
   const sticky = data.stickied || false;
+  const [copied, setCopied] = useState(false);
 
   if (!visible) {
     return (
@@ -21,6 +23,15 @@ const PostFooter = ({ debug, visible, toggleShowDebug }) => {
     // This is weird and occasionally happens.
     // console.log(data);
   }
+
+  const copyID = comp => {
+    const id = comp.target.textContent;
+    copy(id);
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 500);
+  };
 
   return (
     <footer className="d-flex clearfix align-middle mt-1">
@@ -37,15 +48,31 @@ const PostFooter = ({ debug, visible, toggleShowDebug }) => {
       <div>
         {debug && (
           <span className="pl-3">
-            {data.name}{' '}
             <button
-              className="btn btn-link m-0 p-0"
+              className="btn btn-link m-0 p-0 mr-1"
+              onClick={copyID}
+              title="Click to copy"
+              type="button"
+            >
+              {copied ? 'Copied' : data.name}
+            </button>
+            <Link
+              to={`/r/${
+                data.subreddit
+              }/search?q=${`title:'${data.title}' author:${data.author}`}`}
+              title="Isolate this entry for debugging"
+              className="m-0 mr-1 p-0"
+            >
+              <i className="fas fa-search" />
+            </Link>
+            <button
+              className="btn btn-link m-0 p-0 mr-1"
               onClick={() => toggleShowDebug()}
               title="Show debug"
               type="button"
             >
               <i className="fas fa-code" />
-            </button>{' '}
+            </button>
           </span>
         )}
         {!data.is_self && (
