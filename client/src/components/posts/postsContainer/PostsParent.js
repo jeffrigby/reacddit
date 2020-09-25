a22181acf866a9c9b02a371d8e25f7c455ccc825
Jeffrey Rigby <9286474+jeffrigby@users.noreply.github.com>
2020-09-24 23:40:42 -0400

    Better styling for comment links

--- client/src/components/posts/postsContainer/PostsParent.js
+++ client/src/components/posts/postsContainer/PostsParent.js
@@ -18,25 +18,31 @@ const PostsParent = ({ post }) => {
         idx={0}
       />
     );
-    const subhead =
-      listType === 'comments' ? 'Comments' : 'Duplicate/Cross Posts';
+
+    let subhead;
+    if (listType === 'comments') {
+      subhead = comment ? 'Comment Thread' : 'Comments';
+    } else {
+      subhead = 'Duplicate/Cross Posts';
+    }
 
     let commentLinks;
     if (comment) {
       const parentCommentLink = `${post.data.permalink}${comment}/?context=8&depth=9`;
       commentLinks = (
-        <>
+        <div className="list-actions">
           <Link to={post.data.permalink}>View all comments</Link>{' '}
           <Link to={parentCommentLink}>Show parent comments</Link>
-        </>
+        </div>
       );
     }
 
     return (
       <>
         {parentPost}
-        <div className="list-title">{subhead}</div>
-        {commentLinks}
+        <div className="list-title">
+          {subhead} {commentLinks}
+        </div>
       </>
     );
   }
