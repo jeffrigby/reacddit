8f5102d03e7a53c2faa15751fcee137346d58dfb
Jeffrey Rigby <9286474+jeffrigby@users.noreply.github.com>
2024-06-11 00:29:48 -0400

    Refactor import and return statement in Twitter component

--- client/src/components/posts/embeds/domains/twittercom.js
+++ client/src/components/posts/embeds/domains/twittercom.js
@@ -1,16 +1,15 @@
 import parse from 'url-parse';
-import isNumeric from 'locutus/php/var/is_numeric';
+import { isNumeric } from '../../../../common';
 
 const render = (entry) => {
   const parsedUrl = parse(entry.url, true);
   const id = parsedUrl.pathname.split('/').pop();
 
   if (isNumeric(id)) {
-    const content = {
+    return {
       type: 'twitter',
       id,
     };
-    return content;
   }
 
   return null;
