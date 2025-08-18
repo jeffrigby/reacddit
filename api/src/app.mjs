f9538b3ea1437296520cabc852f9aca5cfbdd728
Jeffrey Rigby <9286474+jeffrigby@users.noreply.github.com>
2025-08-18 08:30:12 -0400

    Package updates

--- api/src/app.mjs
+++ api/src/app.mjs
@@ -1,5 +1,5 @@
 import Koa from "koa";
-import Router from "koa-router";
+import Router from "@koa/router";
 import session from "koa-session";
 import logger from "koa-logger";
 import { randomUUID } from "crypto";
